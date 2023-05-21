6846b7166580d7bd6d779bd4a27181b00e8a51d2
danielvt.softdev@gmail.com <danielvt.softdev@gmail.com>
2023-05-21 14:57:40 +0200

    Added scripts for either creating or restoring a database.

---
+++ SupplierDatabase/dbo/Tables/Products.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductID]   INT             NOT NULL,
+    [SupplierID]  BIGINT          NULL,
+    [ProductName] VARCHAR (255)   NULL,
+    [Description] VARCHAR (255)   NULL,
+    [Price]       DECIMAL (10, 2) NULL,
+    [DateCreated] DATETIME        NULL,
+    [DateUpdated] DATETIME        NULL,
+    PRIMARY KEY CLUSTERED ([ProductID] ASC),
+    FOREIGN KEY ([SupplierID]) REFERENCES [dbo].[Suppliers] ([SupplierId])
+);
+
